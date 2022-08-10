998b84d83d3d5f60c1f62017114c5cc334dc6f2d
MuhammadSaeedSaad <mouhammedsaed@gmail.com>
2022-08-10 23:51:33 +0200

    added a test for ExpenseForm for an invalid submit

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,12 @@ test("should render ExpenseForm with sample data from fixtures", () => {
     const wrapper = shallow(<ExpenseForm expense={expenses[0]} />);
     expect(wrapper).toMatchSnapshot();
 });
+
+test("should render ExpenseForm with an error when submitted without data", () => {
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find("form").simulate("submit", { 
+        preventDefault: () => {}
+    });
+    expect(wrapper.state("error").length).toBeGreaterThan(0);
+    expect(wrapper).toMatchSnapshot();
+})
